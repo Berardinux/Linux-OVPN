26da8cf9b5caf2a31d5541e7f2a2fc93d48ce546
Berardinux <berardinux@gmail.com>
2025-06-10 20:01:00 -0400

    20250610-2000

--- src/window_components/imported_profile_window_components.py
+++ src/window_components/imported_profile_window_components.py
@@ -13,6 +13,7 @@ class ImportedProfileWindowUIComponents:
         self.footer_box = None
         self.config=ReadWriteJSON().read_config()
         self.theme = self.config.get("theme", "light")
+        self.password_row_cache = 0
 
     def create_imported_profile_header_box(self, callback):
         self.header_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
@@ -47,12 +48,81 @@ class ImportedProfileWindowUIComponents:
         return self.header_box
 
     def create_imported_profile_body_box(self):
-        self.body_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+        self.body_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        self.body_box.set_halign(Gtk.Align.START)
+        self.body_box.set_valign(Gtk.Align.START)
+        self.body_box.set_margin_top(20)
+        self.body_box.set_margin_left(40)
+        self.body_box.set_margin_right(40)
         self.body_box.set_name("custom-body")
 
+        profile_name = Gtk.Label(label="Profile Name")
+        profile_name.get_style_context().add_class("h6")
+        profile_name.get_style_context().add_class("color1")
+        profile_name.set_halign(Gtk.Align.START)
+        self.body_box.pack_start(profile_name, False, False, 0)
 
-        
+        entry_profile_name = Gtk.Entry()
+        entry_profile_name.set_text("berardinux.com [Berardinux]")
+        entry_profile_name.get_style_context().add_class("entry")
+        entry_profile_name.set_size_request(420, -1)
+        self.body_box.pack_start(entry_profile_name, False, False, 0)
+
+        server_name = Gtk.Label(label="Server Hostname (locked)")
+        server_name.get_style_context().add_class("h6")
+        server_name.get_style_context().add_class("color1")
+        server_name.set_halign(Gtk.Align.START)
+        server_name.set_margin_top(20)
+        self.body_box.pack_start(server_name, False, False, 0)
+
+        entry_server_name = Gtk.Entry()
+        entry_server_name.set_text("berardinux.com")
+        entry_server_name.set_editable(False)
+        entry_server_name.get_style_context().add_class("entry")
+        self.body_box.pack_start(entry_server_name, False, False, 0)
+
+        check_button_save_private_passwd = Gtk.CheckButton(label="Save Private Key Password")
+        check_button_save_private_passwd.set_active(False)
+        check_button_save_private_passwd.get_style_context().add_class("h6")
+        check_button_save_private_passwd.get_style_context().add_class("color1")
+        check_button_save_private_passwd.set_margin_top(20)
+        check_button_save_private_passwd.connect("toggled", self.on_checkbox_toggled)
+        self.body_box.pack_start(check_button_save_private_passwd, False, False, 0)
+
+        self.password_row = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=5)
+
+        self.entry_private_key_password = Gtk.Entry()
+        self.entry_private_key_password.set_placeholder_text("Private Key Password")
+        self.entry_private_key_password.set_visibility(False)
+        self.entry_private_key_password.set_invisible_char("•")
+        self.entry_private_key_password.get_style_context().add_class("entry")
+        self.entry_private_key_password.set_size_request(380, -1)
+
+        self.toggle_visibility_btn = Gtk.Button()
+        eye_icon = Gtk.Image.new_from_icon_name("view-reveal-symbolic", Gtk.IconSize.BUTTON)
+        self.toggle_visibility_btn.set_image(eye_icon)
+        self.toggle_visibility_btn.set_relief(Gtk.ReliefStyle.NONE)
+        self.toggle_visibility_btn.connect("clicked", self.on_toggle_password_visibility)
+
+        self.body_box.pack_start(self.password_row, False, False, 0)
+
         return self.body_box
+
+    def on_toggle_password_visibility(self, button):
+        current = self.entry_private_key_password.get_visibility()
+        self.entry_private_key_password.set_visibility(not current)
+
+    def on_checkbox_toggled(self, button):
+        if button.get_active():
+            if self.password_row_cache == 0:
+                self.password_row.pack_start(self.entry_private_key_password, False, False, 0)
+                self.password_row.pack_start(self.toggle_visibility_btn, False, False, 0)
+            else:
+                self.password_row.show_all()
+        else:
+            self.password_row.hide()
+
+        self.password_row_cache += 1
 
     def create_imported_profile_footer_box(self):
         self.footer_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
